045906594a53a7f9d4168fce6f4bee6e069598ad
Avi1702 <avinashroy1702@gmail.com>
2023-01-07 14:36:38 +0530

    Home page top section done

--- masai/src/App.js
+++ masai/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import Home from './pages/Home';
+import {Home} from './pages/Home';
 import Navbar from "./pages/Navbar"
 
 function App() {
   return (
     <div className="App">
    <Navbar/>
+   <Home/>
     </div>
   );
 }
--- masai/src/pages/Home.jsx
+++ masai/src/pages/Home.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import Box from '@mui/material/Box';
+// import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 
 export const Home = () => {
   return (
-    <div>
+    <div >
          <Box
       sx={{
-        width: 300,
-        height: 300,
-        backgroundColor: 'primary.dark',
-        '&:hover': {
-          backgroundColor: 'primary.main',
-          opacity: [0.9, 0.8, 0.7],
-        },
+        width:"40%",
+        margin:"auto"
+
+      
       }}
-    />
+    >
+       <h1 style={{fontSize:"43px",fontWeight:"800"}}>Driven by Outcomes, Fuelled by Ambitions.</h1> 
+       <p  style={{fontSize:"25px"}}>Launch your dream career<h3 style={{marginTop:"0px"}}>at 0 upfront fee.</h3></p>
+    
+       <img src="https://www.masaischool.com/img/homepage/scribble.png" alt="scrubling" style={{position:"absolute",margin:"-3.5%",left:"45%"}}></img><br/>
+       <Button sx={{backgroundColor:"red",'&:hover':{backgroundColor:"#DB4747"}}} variant="contained">APPLY NOW FOR FREE</Button>
+        </Box>
     </div>
   )
 }
